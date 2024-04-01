84f9d8507821c0f46876aa863c5472811242719e
CarsonH19 <questline.carson@gmail.com>
2024-04-01 23:48:35 +0900

    feat: form reset logic

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,8 @@ export default function Signup() {
     const data = Object.fromEntries(fd.entries());
     data.acquisitionChannel = acquisitionChannel;
     console.log(data);
+
+    event.target.reset();
   }
 
   return (
